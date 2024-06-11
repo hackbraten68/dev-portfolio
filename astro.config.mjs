09e021ca746d824fbcc7a6ae61fdef651f4912ad
Sam Dillenburg <39334877+hackbraten68@users.noreply.github.com>
2024-06-11 14:12:50 +0000

    integrating a blog system

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon(), mdx(), sitemap()],
+  content: {
+    collections: {
+      blog: {
+        // Definieren Sie hier Ihre Sammlungskonfiguration, falls erforderlich
+      },
+    },
+  },
 });
