c88815934354c4e2252c118dfa27a9b1fb2535fd
Sam Dillenburg <39334877+hackbraten68@users.noreply.github.com>
2024-06-11 12:06:47 +0200

    testing few things with new mono-font and avatar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        'menlo': ['Menlo Regular', 'monospace'],
+      },
       colors: {
         // Background
         synthwaveNachtBlau: '#190E32',
